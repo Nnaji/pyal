d09fb2160839e9b3615a6d25c55b9f5c7e1d49e9
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-09-10 12:08:39 +0200

    palindrome testcase created

--- test/test.spec.py
+++ test/test.spec.py
@@ -8,11 +8,15 @@ import sys
 import unittest
 sys.path.append("..")
 import iseven
+import palindrome
 
 class MyUnitTest(unittest.TestCase):
     def test_iseven_function(self):
         self.assertTrue(iseven.is_Even(10), "It should return true")
         self.assertFalse(iseven.is_Even(3), "It should return false")
+
+    def test_isPalindrome_function(self):
+        self.assertTrue(palindrome.is_palindrome("noon"), "Noon is a Palindrom")
 
 if __name__=="__main__":
     unittest.main()
